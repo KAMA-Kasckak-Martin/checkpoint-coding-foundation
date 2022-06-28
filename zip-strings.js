080e2b9a7f1300866e4bd4cf18ca6a529c33020f
Martin Kascak <m.kascak93@gmail.com>
2022-06-28 15:15:24 +0200

    feat: finish

--- zip-strings.js
+++ zip-strings.js
@@ -1,3 +1,14 @@
 function zipStrings(strA, strB) {
-  // Add you solution here
+  let mergedArr = [];
+  let arrStrA = strA.split("");
+  let arrStrB = strB.split("");
+  const length = Math.max(arrStrA.length, arrStrB.length);
+
+  for (let i = 0; i < length; i++) {
+    mergedArr.push(arrStrA[i]);
+    mergedArr.push(arrStrB[i]);
+  }
+  return mergedArr.join("");
 }
+
+//console.log(zipStrings("", "1"));
